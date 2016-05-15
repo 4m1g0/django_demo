060946876c298d5a3ca54bb4ac68d5e91c8fb90c
4m1g0 <dev.4m1g0@gmail.com>
2016-05-15 22:46:38 +0200

    Access data models from the view, and add some mock data

--- demo/gallery/views.py
+++ demo/gallery/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Gallery
 
 def index(request):
-    return HttpResponse("This is our first view!")
+    galleries = Gallery.objects.all()
+    html = ''
+    for gallery in galleries:
+        url = '/gallery/' + str(gallery.id) + '/'
+        html += '<a href="' + url + '">' + gallery.name + '</a><br>'
+        
+    return HttpResponse(html)
     
 def detail(request, id):
     return HttpResponse("This is the details page of gallery: " + str(id))
