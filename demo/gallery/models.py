353d2b77e0d4db11cf9ee677de556b9c7a60b543
4m1g0 <dev.4m1g0@gmail.com>
2016-05-15 21:39:02 +0200

    Add models for gallery and makemigrations and migrate

--- demo/gallery/models.py
+++ demo/gallery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Gallery(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.CharField(max_length=255)
+
+class Photo(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.CharField(max_length=255)
+    gallery = models.ForeignKey(Gallery)
+    
