0bd6e7d1646a7acf8434a21d8d8feca2f3a598a3
4m1g0 <dev.4m1g0@gmail.com>
2016-05-15 23:01:48 +0200

    Using the first template (in the next commit we will rebuild it in a better way

--- demo/gallery/views.py
+++ demo/gallery/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Gallery
+from django.template import loader
+
+#def index(request):
+#    galleries = Gallery.objects.all()
+#    html = ''
+#    for gallery in galleries:
+#        url = '/gallery/' + str(gallery.id) + '/'
+#        html += '<a href="' + url + '">' + gallery.name + '</a><br>'
+#        
+#    return HttpResponse(html)
 
 def index(request):
     galleries = Gallery.objects.all()
-    html = ''
-    for gallery in galleries:
-        url = '/gallery/' + str(gallery.id) + '/'
-        html += '<a href="' + url + '">' + gallery.name + '</a><br>'
-        
-    return HttpResponse(html)
+    template = loader.get_template('gallery/index.html')
+    context = {
+        'galleries' : galleries
+    }
     
+    return HttpResponse(template.render(context, request))
+    
+
 def detail(request, id):
     return HttpResponse("This is the details page of gallery: " + str(id))
